a9fdf18360bc185b043260f767a74c6862b0c840
Ceejayski <sirchijioke1@gmail.com>
2022-02-22 18:54:19 +0100

    Remove redundant styles

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { ToastContainer } from 'react-toastify';
-import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'react-toastify/dist/ReactToastify.css';
 import store from './redux';
